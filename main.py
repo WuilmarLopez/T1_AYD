e49cfd438f477fab843c61b562dc1d267e34a63d
Wuilmar Hamilton López García <3658871100115@ingenieria.usac.edu.gt>
2023-08-02 21:22:38 -0600

    gitkraken agrego contenido al archivo

--- main.py
+++ main.py
@@ -16,11 +16,8 @@ def index():
 @app.post("/post")
 def post(numero: numeros):
     return {"suma": f"{numero.numero1 + numero.numero2}"}
-<<<<<<< HEAD
-=======
 
 #poniendo el metodo de info
 @app.get("/info")
 def info():
     return {"info": "201901069"}
->>>>>>> info-201901069
