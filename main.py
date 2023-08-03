e22d46792fa523e72f43a0073062a970e5faa7a6
Wuilmar Hamilton López García <3658871100115@ingenieria.usac.edu.gt>
2023-08-02 20:55:36 -0600

    se creo el método info para mostrar el carne

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class numeros(BaseModel):
+    numero1: int
+    numero2: int
+
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
+
+
+#poniendo el metodo de suma
+@app.post("/post")
+def post(numero: numeros):
+    return {"suma": f"{numero.numero1 + numero.numero2}"}
+
+#poniendo el metodo de info
+@app.get("/info")
+def info():
+    return {"info": "201901069"}
